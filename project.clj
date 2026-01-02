24ef91fe3d4ea5f2d4d35cf705dfaaa8a772105d
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-01-02 23:58:00 +0000

    Version 1.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/comidi "1.1.0"
+(defproject org.openvoxproject/comidi "1.1.1-SNAPSHOT"
   :description "OpenVox utility functions and compojure-like wrappers for use with the bidi web routing library"
   :url "https://github.com/openvoxproject/comidi"
   :license {:name "Apache License, Version 2.0"
