e30a9a74e90471076195077cba25e56c4f8dde4c
nmburgan <13688219+nmburgan@users.noreply.github.com>
2026-01-09 01:07:19 -0800

    Remove clj-parent
    In an effort to simplify our projects, this removes clj-parent and specifies dependency versions directly. Since we only have two top-level components (openvox-server and openvoxdb) and we use renovate to keep dependencies up to date, this introduces less churn and coordination when dependencies need updating.

--- project.clj
+++ project.clj
@@ -8,19 +8,21 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [org.openvoxproject/clj-parent "7.6.3"]
-                   :inherit [:managed-dependencies]}
+  ;; These are to enforce consistent versions across dependencies of dependencies,
+  ;; and to avoid having to define versions in multiple places. If a component
+  ;; defined under :dependencies ends up causing an error due to :pedantic? :abort,
+  ;; because it is a dep of a dep with a different version, move it here.
+  :managed-dependencies [[org.clojure/clojure "1.12.4"]
+                         [commons-io "2.20.0"]]
 
   :dependencies [[org.clojure/clojure]
 
-                 [bidi]
-                 [compojure]
-                 [prismatic/schema]
-                 [org.openvoxproject/kitchensink]]
+                 [bidi "2.1.6"]
+                 [compojure "1.7.1"]
+                 [prismatic/schema "1.1.12"]
+                 [org.openvoxproject/kitchensink "3.5.3"]]
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/CLOJARS_USERNAME
                                      :password :env/CLOJARS_PASSWORD
-                                     :sign-releases false}]]
-
-  :plugins  [[lein-parent "0.3.9"]])
+                                     :sign-releases false}]])
